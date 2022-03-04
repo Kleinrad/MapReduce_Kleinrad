de42ba663b84a97edebebfe629cc408e21747ef8
Kleinrad <fabiankleinrad.fk@gmail.com>
2022-03-04 03:40:01 -0800

    fixed message types

--- include/protoutils.hpp
+++ include/protoutils.hpp
@@ -6,21 +6,21 @@
 
 mapreduce::WorkerAssignment generateWorkerAssignment(int id){
     mapreduce::WorkerAssignment assignment;
-    assignment.set_type(mapreduce::MessageType::worker_assignment);
+    assignment.set_type(mapreduce::MessageType::WORKER_ASSIGNMENT);
     assignment.set_worker_id(id);
     return assignment;
 }
 
 mapreduce::WorkerSignOff generateWorkerSignOff(int id){
     mapreduce::WorkerSignOff signOff;
-    signOff.set_type(mapreduce::MessageType::worker_sign_off);
+    signOff.set_type(mapreduce::MessageType::WORKER_SIGN_OFF);
     signOff.set_worker_id(id);
     return signOff;
 }
 
 mapreduce::Confirm generateConfirm(int id){
     mapreduce::Confirm confirm;
-    confirm.set_type(mapreduce::MessageType::confirm);
+    confirm.set_type(mapreduce::MessageType::CONFIRM);
     confirm.set_worker_id(id);
     return confirm;
 }
